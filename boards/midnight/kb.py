f050fa5d4e8729cf68cd90efcb6a36227cea2787
Nathan Crapo <nathan_crapo@yahoo.com>
2022-11-20 10:39:20 -0700

    Got both sides initially sorted

--- boards/midnight/kb.py
+++ boards/midnight/kb.py
@@ -1,36 +1,58 @@
-# Nathan Crapo's Midnight board
+# Nathan Crapo's Midnight board. This file is for "hardware setup" -
+# scanning, split operation etc.
 
 import board
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
+from kmk.modules.split import Split, SplitSide
 from storage import getmount
 
+# Must change this per side. Could we create a new file to contain it maybe?
+leftSide = True
+
 # fmt: off
+# The right is a mirror image of the left side
 _KEY_CFG_LEFT = [
     board.GP6,  board.GP9,  board.GP12, board.GP15, board.GP26, board.GP20,
     board.GP5,  board.GP8,  board.GP11, board.GP14, board.GP27, board.GP21,
     board.GP4,  board.GP7,  board.GP10, board.GP13, board.GP28, board.GP22,
-                                        board.GP19, board.GP18, board.GP17
-]
-
-_KEY_CFG_RIGHT = [
-    board.GP20, board.GP26, board.GP15, board.GP12, board.GP9,  board.GP6,
-    board.GP21, board.GP27, board.GP14, board.GP11, board.GP8,  board.GP5,
-    board.GP22, board.GP28, board.GP13, board.GP10, board.GP7,  board.GP4,
-    board.GP16, board.GP18, board.GP19
+                                        board.GP19, board.GP18, board.GP2,
 ]
 # Fmt: on
+
+# Note: might eventually change the Split obj - set uart_flip=False
+# and define data_pin=GP17 (RX), data_pin2=GP16 (TX). But need to
+# identify and provide side or let Split auto-detect side. Split
+# determines side by looking at the last char of mount name 'L' is
+# left, 'R' is right, BUT then 'target' is only defined by if USB is
+# connected or not. Connecting USB to the wrong side would yield a
+# non-working setup.
+
+if (leftSide):
+    split = Split(
+        data_pin=board.GP16,
+        split_side=SplitSide.LEFT,
+        split_target_left=False,
+    )
+else:
+    split = Split(
+        data_pin=board.GP17,
+        split_side=SplitSide.RIGHT,
+        split_target_left=False,
+    )
 
 class KMKKeyboard(_KMKKeyboard):
     def __init__(self):
         # create and register the scanner
         self.matrix = KeysScanner(_KEY_CFG_LEFT)
+        self.modules = [ split ]
+
 
     # flake8: noqa
     # fmt: off
     coord_mapping = [
-        0,  1,  2,  3,  4,  5,
-        6,  7,  8,  9,  10, 11,
-        12, 13, 14, 15, 16, 17,
-                    18, 19, 20,
+        0,  1,  2,  3,  4,  5,     26, 25, 24, 23, 22, 21,
+        6,  7,  8,  9,  10, 11,    32, 31, 30, 29, 28, 27,
+        12, 13, 14, 15, 16, 17,    38, 37, 36, 35, 34, 33,
+                    18, 19, 20,    41, 40, 39,
     ]
--- boards/midnight/main.py
+++ boards/midnight/main.py
@@ -1,3 +1,4 @@
+# Higher level Keyboard config like layers
 import board
 from kb import KMKKeyboard
 
@@ -6,20 +7,15 @@ from kmk.modules.layers import Layers
 
 keyboard = KMKKeyboard()
 
+
 # fmt: off
 # flake8: noqa
 keyboard.keymap = [
-#    [  # QWERTY - Right
-#        KC.Y, KC.U, KC.I, KC.O, KC.P, KC.BACKSLASH,
-#        KC.H, KC.J, KC.k, KC.L, KC.SEMICOLON, KC.QUOTE,
-#        KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH, KC.LSFT,
-#        KC.LALT, KC.ENTER, KC.LCTL,
-#    ]
-    [  # QWERTY - Left
-        KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T,
-        KC.LGUI, KC.A, KC.S, KC.D, KC.F, KC.G,
-        KC.LSFT, KC.Z, KC.X, KC.C, KC.V, KC.B,
-                             KC.BACKSPACE, KC.LCTL, KC.LALT,
+    [  # QWERTY - Right
+        KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T,            KC.Y, KC.U, KC.I, KC.O, KC.P, KC.BACKSLASH,
+        KC.LGUI, KC.A, KC.S, KC.D, KC.F, KC.G,           KC.H, KC.J, KC.k, KC.L, KC.SEMICOLON, KC.QUOTE,
+        KC.LSFT, KC.Z, KC.X, KC.C, KC.V, KC.B,           KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH, KC.LSFT,
+                   KC.BACKSPACE, KC.LCTL, KC.LALT,    KC.LALT, KC.ENTER, KC.LCTL,
     ]
 ]
 

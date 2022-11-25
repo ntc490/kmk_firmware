732e65eca5ccf7894b305763cef44c4d192b85f4
Nathan Crapo <nathan_crapo@yahoo.com>
2022-11-24 23:40:50 -0700

    Add colemak layer and method to switch default layer

--- boards/midnight/code.py
+++ boards/midnight/code.py
@@ -14,10 +14,11 @@ keyboard.modules.extend([ Layers(), ModTap(), OneShot() ])
 # --------------- Key aliases ---------------
 
 QWERTY_LAYER = 0
-NUM_LAYER = 1
-PWR_LAYER = 2
-FUNC_LAYER = 3
-MOUSE_LAYER = 4
+COLEMAK_LAYER = 1
+NUM_LAYER = 2
+PWR_LAYER = 3
+FUNC_LAYER = 4
+MOUSE_LAYER = 5
 
 ______ = KC.TRNS
 XXXXXX = KC.NO
@@ -42,6 +43,12 @@ keyboard.keymap = [
         KC.LSFT, FUNC_Z,  KC.X,    KC.C,    KC.V,    KC.B,             KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH,     KC.OS(KC.LM(NUM_LAYER, KC.LSFT)),
                                KC.BACKSPACE, KC.LCTL, KC.LALT,      KC.LALT, LGUI_ENTER, HYPR_SPC,
     ],
+    [  # Colemak
+        PWR_ESC, KC.Q,    KC.W,    KC.F,    KC.P,    KC.B,             KC.J, KC.L, KC.U,     KC.Y,   KC.SEMICOLON, KC.BSLASH,
+        NUM_TAB, KC.A,    KC.R,    KC.S,    KC.T,    KC.G,             KC.M, KC.N, KC.E,     KC.I,   KC.O,         KC.QUOTE,
+        KC.LSFT, FUNC_Z,  KC.X,    KC.C,    KC.D,    KC.V,             KC.K, KC.H, KC.COMMA, KC.DOT, KC.SLASH,     KC.OS(KC.LM(NUM_LAYER, KC.LSFT)),
+                               KC.BACKSPACE, KC.LCTL, KC.LALT,      KC.LALT, LGUI_ENTER, HYPR_SPC,
+    ],
     [  # Numbers
         KC.ESC,  XXXXXX,  XXXXXX, XXXXXX, KC.LCBR, KC.RCBR,            KC.EQUAL, KC.N7, KC.N8, KC.N9, KC.UNDS,  ______,
         ______,  KC.TILD, XXXXXX, XXXXXX, KC.LPRN, KC.RPRN,            KC.MINUS, KC.N4, KC.N5, KC.N6, KC.PLUS,  ______,
@@ -56,7 +63,7 @@ keyboard.keymap = [
     ],
     [  # Function keys
         XXXXXX,  XXXXXX,  XXXXXX, XXXXXX, XXXXXX, XXXXXX,              KC.HOME,  KC.F7,   KC.F8,  KC.F9,  KC.F10,  KC.DEL,
-        XXXXXX,  XXXXXX,  XXXXXX, XXXXXX, XXXXXX, XXXXXX,              KC.END,   KC.F4,   KC.F5,  KC.F6,  KC.F11,  KC.PSCREEN,
+        XXXXXX,  XXXXXX,  XXXXXX, XXXXXX, KC.DF(QWERTY_LAYER), KC.DF(COLEMAK_LAYER),              KC.END,   KC.F4,   KC.F5,  KC.F6,  KC.F11,  KC.PSCREEN,
         XXXXXX,  XXXXXX,  XXXXXX, KC.CAPS,XXXXXX, XXXXXX,              KC.INS,   KC.F1,   KC.F2,  KC.F3,  KC.F12,  C_A_DEL,
                                         ______, ______, ______,    ______, ______, ______,
     ],

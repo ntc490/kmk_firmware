a6918ce8ea3cde08200609b0fb6ad143afa37497
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2024-08-01 20:35:58 -0600

    Rough in Kine3 keyboard. Partially works with right side.

---
+++ boards/kine3/README.md
@@ -0,0 +1,4 @@
+# Kine3
+
+The [Kine3]() is a hand-wired, split, 46 key keyboard. It's inspired
+by Kinesis Advantage and other similar boards.
---
+++ boards/kine3/code.py
@@ -0,0 +1,52 @@
+# Higher level Keyboard config like layers
+#
+# Keymappings adapted from:
+# https://peterxjang.com/blog/designing-a-36-key-custom-keyboard-layout.html
+#
+# QWERTY [base layer]
+# ,-----.-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.-----.
+# | TAB | q   | w   | e   | r   | t   |                      | y   | u   | i   | o   | p   | \ | |
+# |-----+-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----+-----|
+# | NC  | a   | s   | d   | f   | g   |                      | h   | j   | k   | l   | ; : | ' " |
+# |-----+-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----+-----|
+# | LSFT| z   | x   | c   | v   | b   |                      | n   | m   | , < | . > | / ? | RSFT|
+# `-----.-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----+-----'
+#               .-------.-------.-------.      .-------.-------.-------.
+#               | BKSPC |  CTRL | ALT   |      |  ALT  | ENTER |  SPC  |
+#               '-------'-------'-------'      '-------'-------'-------'
+#
+
+import board
+from kb import KMKKeyboard, Mapper42, add_keyboard_layer
+from kmk.keys import KC
+from kmk.modules.layers import Layers
+
+keyboard = KMKKeyboard()
+
+#keyboard.modules.extend([ Layers(), ])
+
+# --------------- Layer Indexes ---------------
+
+qwerty = Mapper42()
+
+# --------------- Key Definitions and Aliases ---------------
+
+
+# --------------- Key maps ---------------
+
+keyboard.keymap = [
+[
+    KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,         KC.BACKSLASH,
+    KC.H,       KC.J,       KC.K,       KC.L,       KC.SEMICOLON, KC.QUOTE,
+    KC.N,       KC.M,       KC.COMMA,   KC.DOT,     KC.SLASH,     KC.RSHIFT,
+    KC.RALT, KC.ENTER,   KC.SPACE
+]
+]
+
+# fmt: off
+# flake8: noqa
+#add_keyboard_layer(keyboard,
+#                   qwerty,)
+
+if __name__ == "__main__":
+    keyboard.go()

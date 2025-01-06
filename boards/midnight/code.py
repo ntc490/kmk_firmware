0ba11049d3a7f882e39c238741db7154cdf662c8
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2025-01-05 22:19:57 -0700

    Very basic 42 key mapping for Midnight keyboard

--- boards/midnight/code.py
+++ boards/midnight/code.py
@@ -1,278 +1,38 @@
 # Higher level Keyboard config like layers
-#
-# Keymappings adapted from:
-# https://peterxjang.com/blog/designing-a-36-key-custom-keyboard-layout.html
-#
-# QWERTY [base layer]
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# | q   | w   | e   | r   | t   |                      | y   | u   | i   | o   | p   |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# | a   | s   | d   | f   | g   |                      | h   | j   | k   | l   | ; : |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |SHF/z| x   | c   | v   | b   |                      | n   | m   | , < | . > |SHF/?|
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               |GUI/BSP|CTR/TAB|ALT/*TD|      |ALT/HYP| ENTER |NUM/SPC|
-#               '-------'-------'-------'      '-------'-------'-------'
-# *TD: tap2=PWR, tap3=FKEYS
-#
-# Numbers
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# | 1 ! | 2 @ | 3 # | 4 $ | 5 % |                      | 6 ^ | 7 & | 8 * | 9 ( | 0 ) |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# | ` ~ |HOME |PGUP |PGDN | END |                      |LEFT |DOWN | UP  |RGHT | ' " |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |LSHFT| = + | [ { | ] } | - _ |                      |NXTWN| XXX |     |     |RSHFT|
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               |       |       |       |      |       |       | (hold)|
-#               '-------'-------'-------'      '-------'-------'-------'
-#
-# Power
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# | ESC | XXX |KEBAB|  (  |  )  |                      |  =  | 7 & | 8 * | 9 ( | \ | |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# | TAB |  ~  |USNAK|  {  |  }  |                      | - _ | 4 $ | 5 % | 6 ^ | ' " |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |LSHFT|CAMEL|SNAKE|  [  |  ]  |                      | . > | 1 ! | 2 @ | 3 # |RSHFT|
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               | BKSP  |       |(hold2)|      |       |       |  0 )  |
-#               '-------'-------'-------'      '-------'-------'-------'
-#
-# Function keys
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# | F1  | F2  | F3  | F4  | F5  |                      | F6  | F7  | F8  | F9  | F10 |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# | F11 | F12 | XXX | XXX | XXX |                      |MLEFT|MDOWN| MUP |MRGHT| XXX |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |LSHFT| XXX |CAPS |QWERT|COLEM|                      |BTN1 | BTN2| XXX | XXX |RSHFT|
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               | BKSP  |       |(hold3)|      |       |       | SPACE |
-#               '-------'-------'-------'      '-------'-------'-------'
-#
-# Uppercase Snake
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# | Q   | W   | E   | R   | T   |                      | Y   | U   | I   | O   | P   |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# | A   | S   | D   | F   | G   |   (ucase letters)    | H   | J   | K   | L   |     |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |     | X   | C   | V   | B   |                      | N   | M   |     |     |     |
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               |       |       | EXIT  |      |       |       |   _   |
-#               '-------'-------'-------'      '-------'-------'-------'
-#
-# Snake
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               |       |       | EXIT  |      |       |       |   _   |
-#               '-------'-------'-------'      '-------'-------'-------'
-#
-# Camel
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               |       |       | EXIT  |      |       |       |OS(SFT)|
-#               '-------'-------'-------'      '-------'-------'-------'
-#
-# Kebab
-# ,-----.-----.-----.-----.-----.                      ,-----.-----.-----.-----.-----.
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# |-----+-----+-----+-----+-----|                      |-----+-----+-----+-----+-----|
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# |-----+-----+-----+-----+-----+                      |-----+-----+-----+-----+-----|
-# |     |     |     |     |     |                      |     |     |     |     |     |
-# `-----'-----'-----'-----'-----'                      `-----'-----'-----'-----'-----'
-#               .-------.-------.-------.      .-------.-------.-------.
-#               |       |       | EXIT  |      |   -   |
-#               '-------'-------'-------'      '-------'-------'-------'
-
 import board
-from kb import KMKKeyboard, Mapper36, add_keyboard_layer
+from kb import KMKKeyboard, Mapper42, add_keyboard_layer
 from kmk.keys import KC
 from kmk.modules.layers import Layers
-from kmk.modules.modtap import ModTap
-from kmk.modules.oneshot import OneShot
-from kmk.modules.mouse_keys import MouseKeys
-from kmk.modules.tapdance import TapDance
 
 keyboard = KMKKeyboard()
 
-keyboard.modules.extend([ Layers(), ModTap(), OneShot(), MouseKeys(), TapDance() ])
+#keyboard.modules.extend([ Layers() ])
 
 # --------------- Layer Indexes ---------------
 
-qwerty = Mapper36()
-numbers = Mapper36()
-power = Mapper36()
-fkeys = Mapper36()
-upper_snake = Mapper36()
-snake = Mapper36()
-camel = Mapper36()
-kebab = Mapper36()
+qwerty = Mapper42()
 
 # --------------- Key Definitions and Aliases ---------------
-
-_______ = KC.TRNS
-XXXXXXX = KC.NO
-NEXTWIN = KC.LGUI(KC.GRAVE)
-QWERTY = KC.DF(qwerty.layer_id)
-LCTRL_TAB = KC.LCTRL(KC.TAB)
-GUI_T = KC.MT(KC.T, KC.LGUI(KC.T), prefer_hold=False, tap_time=300)
-GUI_X = KC.MT(KC.X, KC.LGUI(KC.X), prefer_hold=False, tap_time=300)
-GUI_C = KC.MT(KC.C, KC.LGUI(KC.C), prefer_hold=False, tap_time=300)
-GUI_V = KC.MT(KC.V, KC.LGUI(KC.V), prefer_hold=False, tap_time=300)
-GUI_M = KC.MT(KC.M, KC.LGUI(KC.M), prefer_hold=False, tap_time=300)
-USNAKE_TG = KC.TG(upper_snake.layer_id)
-SNAKE_TG = KC.TG(snake.layer_id)
-CAMEL_TG = KC.TG(camel.layer_id)
-KEBAB_TG = KC.TG(kebab.layer_id)
-
-# Fancy mod-tap/layer-tap multi-function keys
-SHIFT_Z = KC.MT(KC.Z, KC.LSFT, prefer_hold=True)
-SHIFT_BZ = KC.MT(KC.LSFT(KC.Z), KC.LSFT, prefer_hold=True)
-SHIFT_SLASH = KC.MT(KC.SLASH, KC.RSFT, prefer_hold=True)
-SHIFT_BACKSLASH = KC.MT(KC.BACKSLASH, KC.RSFT, prefer_hold=True)
-LGUI_BSP = KC.MT(KC.BKSP, KC.LGUI, prefer_hold=True)
-CTRL_TAB = KC.MT(KC.TAB, KC.LCTRL, prefer_hold=True)
-ALT_PWR = KC.TD(KC.LALT, KC.MO(power.layer_id), KC.MO(fkeys.layer_id))
-ALT_HYPR = KC.TD(KC.LALT, KC.HYPR)
-NUM_SPACE = KC.LT(numbers.layer_id, KC.SPACE, prefer_hold=False)
 
 # --------------- Key maps ---------------
 
 qwerty.left(
-    KC.Q,       KC.W,       KC.E,       KC.R,       GUI_T,
-    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,
-    SHIFT_Z,    GUI_X,      GUI_C,      GUI_V,      KC.B,
-                            LGUI_BSP,   CTRL_TAB,   ALT_PWR
+    KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,
+    KC.LCTRL,   KC.A,       KC.S,       KC.D,       KC.F,       KC.G,
+    KC.LSFT,    KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,
+                                      KC.BKSP,   KC.LCTRL,   KC.LALT
 )
 qwerty.right(
-    KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,
-    KC.H,       KC.J,       KC.K,       KC.L,       KC.SEMICOLON,
-    KC.N,       GUI_M,      KC.COMMA,   KC.DOT,     SHIFT_SLASH,
-    ALT_HYPR,   KC.ENTER,   NUM_SPACE
-)
-
-numbers.left(
-    KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5,
-    KC.GRAVE,   KC.HOME,    KC.PGUP,    KC.PGDN,    KC.END,
-    KC.LSFT,    KC.EQUAL,   KC.LBRC,    KC.RBRC,    KC.MINUS,
-                            _______,    _______,    _______
-)
-numbers.right(
-    KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,
-    KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   KC.QUOTE,
-    NEXTWIN,    XXXXXXX,    _______,    _______,    KC.RSFT,
-    _______,    _______,    _______
-)
-
-power.left(
-    KC.ESC,     XXXXXXX,    KEBAB_TG,   KC.LPRN,    KC.RPRN,
-    KC.TAB,     KC.TILD,    USNAKE_TG,  KC.LCBR,    KC.RCBR,
-    KC.LSFT,    CAMEL_TG,   SNAKE_TG,   KC.LBRC,    KC.RBRC,
-                            KC.BKSP,    _______,    _______
-)
-power.right(
-    KC.EQUAL,   KC.N7,      KC.N8,      KC.N9,      KC.BSLASH,
-    KC.MINUS,   KC.N4,      KC.N5,      KC.N6,      KC.QUOTE,
-    KC.DOT,     KC.N1,      KC.N2,      KC.N3,      KC.RSFT,
-    _______,    _______,    KC.N0
-)
-
-fkeys.left(
-    KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5,
-    KC.F11,     KC.F12,     XXXXXXX,    XXXXXXX,    XXXXXXX,
-    KC.LSFT,    XXXXXXX,    KC.CAPS,    QWERTY,     _______,
-                            KC.BKSP,    _______,    _______
-)
-fkeys.right(
-    KC.F6,      KC.F7,     KC.F8,      KC.F9,      KC.F10,
-    KC.MS_LEFT, KC.MS_DOWN,KC.MS_UP,   KC.MS_RIGHT,XXXXXXX,
-    KC.MB_LMB,  KC.MB_RMB, XXXXXXX,    XXXXXXX,    KC.RSFT,
-    _______,    _______,   KC.SPACE
-)
-
-upper_snake.left(
-    KC.LSFT(KC.Q), KC.LSFT(KC.W), KC.LSFT(KC.E), KC.LSFT(KC.R), KC.LSFT(KC.T),
-    KC.LSFT(KC.A), KC.LSFT(KC.S), KC.LSFT(KC.D), KC.LSFT(KC.F), KC.LSFT(KC.G),
-    SHIFT_BZ,      KC.LSFT(KC.X), KC.LSFT(KC.C), KC.LSFT(KC.V), KC.LSFT(KC.B),
-                                  _______,       _______,       USNAKE_TG
-)
-upper_snake.right(
-    KC.LSFT(KC.Y), KC.LSFT(KC.U), KC.LSFT(KC.I), KC.LSFT(KC.O), KC.LSFT(KC.P),
-    KC.LSFT(KC.H), KC.LSFT(KC.J), KC.LSFT(KC.K), KC.LSFT(KC.L), _______,
-    KC.LSFT(KC.N), KC.LSFT(KC.M), _______,       _______,       _______,
-    _______,       _______,       KC.UNDS
-)
-
-# Change space to underscore - exit with ESC
-snake.left(
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-                            _______,    _______,    SNAKE_TG
-)
-snake.right(
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    KC.UNDS
-)
-
-# Change space to one-shot shift - exit with ESC. Note: 'z' key
-# doesn't work because of MT on 'z' key.
-camel.left(
-    _______,    _______,    _______,    _______,    KC.T,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    KC.X,       KC.C,       KC.V,       _______,
-                            _______,    _______,    CAMEL_TG
-)
-camel.right(
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    KC.M,       _______,    _______,    _______,
-    _______,    _______,    KC.OS(KC.LSFT)
-)
-
-# Change space to dash - exit with ESC
-kebab.left(
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-                            _______,    _______,    KEBAB_TG
-)
-kebab.right(
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,    KC.MINUS
+    KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,         KC.BACKSLASH,
+    KC.H,       KC.J,       KC.K,       KC.L,       KC.SEMICOLON, KC.QUOTE,
+    KC.N,       KC.M,       KC.COMMA,   KC.DOT,     KC.SLASH,     KC.RSFT,
+    KC.LALT,    KC.ENTER,   KC.SPACE
 )
 
 # fmt: off
 # flake8: noqa
 add_keyboard_layer(keyboard,
-                   qwerty,
-                   numbers,
-                   power,
-                   fkeys,
-                   upper_snake,
-                   snake,
-                   camel,
-                   kebab)
+                   qwerty)
 
 if __name__ == "__main__":
     keyboard.go()

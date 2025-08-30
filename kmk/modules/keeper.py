a390763e5279810274bc4421cab41332f435bb87
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2025-08-30 10:11:36 -0600

    Keeper can be enabled or disabled

--- kmk/modules/keeper.py
+++ kmk/modules/keeper.py
@@ -12,6 +12,7 @@ class Keeper(Module):
     def __init__(self):
         self.timeout_ms = 60 * 1000
         self.timer = None
+        self.enabled = False
 
     def during_bootup(self, keyboard):
         return
@@ -24,7 +25,8 @@ class Keeper(Module):
 
     def process_key(self, keyboard, key, is_pressed, int_coord):
         '''Reset the keeper timer so we arent tickling the computer.'''
-        self.reset_timer(keyboard)
+        if self.enabled:
+            self.reset_timer(keyboard)
         return key
 
     def before_hid_send(self, keyboard):
@@ -45,7 +47,13 @@ class Keeper(Module):
         self.timer = keyboard.set_timeout(self.timeout_ms, lambda: self.on_timeout(keyboard))
 
     def on_timeout(self, keyboard):
-        debug('timed out!!')
+        debug('timed out')
+        if not self.enabled:
+            return
+        debug('reset and press shift')
         self.reset_timer(keyboard)
         # Send a shift key press and release
         keyboard.tap_key(KC.LSFT)
+
+    def enable(self, state):
+        self.enabled = state
